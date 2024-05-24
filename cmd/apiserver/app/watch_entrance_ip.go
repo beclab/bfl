5cccb54f6108892a6136769c5a761144383dce6c
eball <liuy102@hotmail.com>
2024-05-24 14:11:17 +0800

    fix: cannot update changed local ip when cloudflare tunnel enabled (#5)
    * fix:  cannot update changed local ip when cloudflare  tunnel enabled
    
    * fix: refresh cloudflare tunnel
    
    ---------
    
    Co-authored-by: liuyu <>

--- cmd/apiserver/app/watch_entrance_ip.go
+++ cmd/apiserver/app/watch_entrance_ip.go
@@ -2,14 +2,19 @@ package app
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"time"
 
 	"bytetrade.io/web3os/bfl/internal/log"
 	"bytetrade.io/web3os/bfl/pkg/apis/iam/v1alpha1/operator"
+	"bytetrade.io/web3os/bfl/pkg/apis/settings/v1alpha1"
 	"bytetrade.io/web3os/bfl/pkg/constants"
 	"bytetrade.io/web3os/bfl/pkg/utils"
 	"bytetrade.io/web3os/bfl/pkg/utils/certmanager"
 	"bytetrade.io/web3os/bfl/pkg/utils/k8sutil"
+	"github.com/emicklei/go-restful"
+	"github.com/go-resty/resty/v2"
 	"github.com/pkg/errors"
 	iamV1alpha2 "kubesphere.io/api/iam/v1alpha2"
 )
@@ -56,13 +61,18 @@ func watch(ctx context.Context) (err error) {
 func reconcile(ctx context.Context, terminusName constants.TerminusName, op *operator.UserOperator, user *iamV1alpha2.User) (err error) {
 	var (
 		isFrp                         bool
+		isCloudFlareTunnel            bool
 		publicDomainIp, localDomainIp string
 	)
 
 	publicDomainIp = op.GetUserAnnotation(user, constants.UserAnnotationPublicDomainIp)
-	if publicDomainIp == "" {
-		log.Warnf("user %q no public domain ip", user.Name)
-		return
+	if publicDomainIp != "" {
+		if utils.ListContains(constants.FrpServers, publicDomainIp) {
+			isFrp = true
+		}
+	} else {
+		// FIXME: assume publicDomainIp is empty cloudflare tunnel should be enabled
+		isCloudFlareTunnel = true
 	}
 
 	localDomainIp = op.GetUserAnnotation(user, constants.UserAnnotationLocalDomainIp)
@@ -71,14 +81,10 @@ func reconcile(ctx context.Context, terminusName constants.TerminusName, op *ope
 		return
 	}
 
-	if utils.ListContains(constants.FrpServers, publicDomainIp) {
-		isFrp = true
-	}
-
 	cm := certmanager.NewCertManager(terminusName)
 
 	var userPatches []func(*iamV1alpha2.User)
-	if !isFrp {
+	if !isFrp && publicDomainIp != "" {
 		// only for public ip
 		publicIp := ""
 		if role, ok := user.Annotations[constants.UserAnnotationOwnerRole]; ok && role == constants.RolePlatformAdmin {
@@ -123,6 +129,47 @@ func reconcile(ctx context.Context, terminusName constants.TerminusName, op *ope
 			u.Annotations[constants.UserAnnotationLocalDomainIp] = *newLocalIp
 		})
 		log.Infof("resolved new local ip: %s", *newLocalIp)
+
+		if isCloudFlareTunnel {
+			// get cloudflare token
+			jws := user.Annotations[constants.UserCertManagerJWSToken]
+			if jws == "" {
+				return errors.Errorf("enable https: user jws not found")
+			}
+
+			terminusName := user.Annotations[constants.UserAnnotationTerminusNameKey]
+			req := v1alpha1.TunnelRequest{
+				Name:    terminusName,
+				Service: fmt.Sprintf("https://%s:%s", *newLocalIp, "443"),
+			}
+
+			res, err := resty.New().SetTimeout(30 * time.Second).R().
+				SetHeaders(map[string]string{
+					restful.HEADER_ContentType: restful.MIME_JSON,
+					restful.HEADER_Accept:      restful.MIME_JSON,
+					"Authorization":            "Bearer " + jws,
+				}).
+				SetBody(req).
+				SetResult(&v1alpha1.TunnelResponse{}).
+				Post("https://terminus-dnsop.snowinning.com/tunnel")
+
+			if err != nil {
+				log.Error("request cloudflare tunnel api error, ", err)
+				return err
+			}
+
+			if res.StatusCode() != http.StatusOK {
+				err = errors.New(string(res.Body()))
+				return err
+			}
+
+			responseData := res.Result().(*v1alpha1.TunnelResponse)
+			if !responseData.Success || responseData.Data == nil || responseData.Data.Token == "" {
+				log.Errorf("get cloudflare tunnel token failed, %v", responseData)
+				err = errors.Errorf("enable https: get cloudflare tunnel token failed")
+				return err
+			}
+		} // end of cloudflare tunnel
 	}
 
 	if err = op.UpdateUser(user, userPatches); err != nil {
