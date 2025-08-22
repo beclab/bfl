e8edba08926fd37dd0b7c4805b8a7e424c8bf485
eball <liuy102@hotmail.com>
2025-08-22 16:45:52 +0800

    fix: add provider header

--- internal/ingress/controllers/patches.go
+++ internal/ingress/controllers/patches.go
@@ -45,6 +45,7 @@ var locationAdditionalsForFilesOp = func(node string) []string {
 		"proxy_set_header X-Real-IP $remote_addr;",
 		"proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;",
 		"proxy_set_header X-Forwarded-Host $host;",
+		"proxy_set_header X-Provider-Proxy $proxy_host;",
 		"client_body_timeout 60s;",
 		"client_max_body_size 2000M;",
 		"proxy_request_buffering off;",
